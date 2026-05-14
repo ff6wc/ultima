b0227584aad977a48b14082387f613dbc1c4c700
wrjones104 <wrjones@gmail.com>
2026-05-13 22:03:54 -0400

    fix button issue

--- packages/ui/Button/Button.tsx
+++ packages/ui/Button/Button.tsx
@@ -9,16 +9,16 @@ type BaseButtonProps = React.DetailedHTMLProps<
 export const buttonStyles = ({ disabled, size = 'default', variant = 'default', className }: { disabled?: boolean | null, size?: 'default' | 'small' | 'smallest' | null, variant?: 'default' | 'primary' | 'danger' | 'discord' | 'outline' | null, className?: string | null }) => {
   return cx(
     styles.button,
-    disabled && styles.disabled,
-    size === 'default' && styles.sizeDefault,
-    size === 'small' && styles.sizeSmall,
-    size === 'smallest' && styles.sizeSmallest,
-    variant === 'default' && styles.variantDefault,
-    variant === 'primary' && styles.variantPrimary,
-    variant === 'danger' && styles.variantDanger,
-    variant === 'discord' && styles.variantDiscord,
-    variant === 'outline' && styles.variantOutline,
-    className
+    disabled ? styles.disabled : undefined,
+    size === 'default' ? styles.sizeDefault : undefined,
+    size === 'small' ? styles.sizeSmall : undefined,
+    size === 'smallest' ? styles.sizeSmallest : undefined,
+    variant === 'default' ? styles.variantDefault : undefined,
+    variant === 'primary' ? styles.variantPrimary : undefined,
+    variant === 'danger' ? styles.variantDanger : undefined,
+    variant === 'discord' ? styles.variantDiscord : undefined,
+    variant === 'outline' ? styles.variantOutline : undefined,
+    className ?? undefined
   );
 };
 
