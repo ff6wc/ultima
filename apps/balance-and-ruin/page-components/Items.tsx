b60024ed157ac33f221f65477c08d084264c1122
jazer <jazer2187@gamil.com>
2026-05-14 12:00:23 -0700

    attempting to get starting items to prepopulate

--- apps/balance-and-ruin/page-components/Items.tsx
+++ apps/balance-and-ruin/page-components/Items.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Chests } from "~/card-components/Chests";
 import { Coliseum } from "~/card-components/Coliseum";
@@ -7,14 +8,20 @@ import { OtherItems } from "~/card-components/OtherItems";
 import { Shops } from "~/card-components/Shops";
 import { StartingGoldAndItems } from "~/card-components/StartingGoldAndItems";
 import { PageContainer } from "~/components/PageContainer/PageContainer";
-import { selectStartingItems } from "~/state/itemSlice";
+import { selectStartingItems, setRawStartingItems } from "~/state/itemSlice";
+import { selectRawFlags } from "~/state/flagSlice";
 
 export const Items = () => {
   const items = useSelector(selectStartingItems);
-  
+
+  const dispatch = useDispatch();
+  const rawFlags = useSelector(selectRawFlags);
+  useEffect(() => {
+    dispatch(setRawStartingItems(rawFlags));
+  }, [dispatch]);
   return (
     <PageContainer columns={2}>
-      <StartingGoldAndItems items={items} curateItems={true}/>
+      <StartingGoldAndItems items={items} curateItems={true} />
       <Shops />
       <Chests />
       <EquipmentPermissions />
