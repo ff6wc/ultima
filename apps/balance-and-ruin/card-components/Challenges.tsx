b8ecea33c12a9f5f81908a89e2439e1c909f3908
Boo <bootherunner@gmail.com>
2026-06-04 07:20:59 -0700

    Add Hide Flags option to challenges card and update metadata

--- apps/balance-and-ruin/card-components/Challenges.tsx
+++ apps/balance-and-ruin/card-components/Challenges.tsx
@@ -7,14 +7,27 @@ export const Challenges = () => {
   return (
     <Card title={"Challenges"}>
       <CardColumn>
-        <FlagSwitch flag="-pd" label="Permadeath" />
+        <FlagSwitch
+          flag="-pd"
+          helperText="No Life spells, Fenix Downs unavailable (except from starting items). Buckets/inns/tents/events do not revive characters. Phoenix casts Life 3 instead of Life"
+          label="Permadeath"
+        />
         <FlagSwitch flag="-bnds" label="Normalize & Distort Boss Stats" />
         <FlagSwitch
           flag="-srp3"
           helperText="Allow Phunbaba 3 to be shuffled and randomized (Otherwise he will only appear in Mobliz WOR)"
           label="Add Phunbaba 3 to Boss Pool"
         />
-        <FlagSwitch flag="-nosaves" label="Ironmog Mode" />
+        <FlagSwitch
+          flag="-nosaves"
+          helperText="You cannot save (save points still work for Tents/Sleeping Bags)"
+          label="Ironmog Mode"
+        />
+        <FlagSwitch
+          flag="-hf"
+          helperText="No log, no flags menu"
+          label="Hide Flags"
+        />
       </CardColumn>
     </Card>
   );
